36a5987cd46893beb7ffefd4cc0dece4b3e5344b
Berkan Karayel <berkankarayel.147@gmail.com>
2025-03-21 17:24:43 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	internal class Content
+	{
+        public int ContentId { get; set; }
+		public string ContentValue { get; set; }
+
+		public DateTime ContentDate { get; set; }
+
+		// Content Yazar 
+		// Content Baslik
+    }
+}
